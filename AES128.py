459553202958094239f8b9148a627885eda6e693
Alex <alexhutman@gmail.com>
2019-10-13 22:35:57 -0400

    fixed naming spacing, and improved some code in main module
    currently not in working condition though

--- AES128.py
+++ AES128.py
@@ -1,91 +1,76 @@
 # TODO: Possibly add the option to do Cipher Block Chaining (CBC) mode?
-
-
-import re
+import hashlib
 import Encrypt
 import Decrypt
 
-xCrypt = None
-message = None
-key = None
+
+def prompt_user():
+    choice = input("Would you like to:\n[1] encrypt\nOR\n[2] decrypt a message?\nEnter here: ")
+    while choice not in ["1", "2"]:
+        input("Please choose\n[1] to encrypt a message\nOR\n[2] to decrypt a message.\nEnter here: ")
+
+    if choice == "1":
+        choice = "encrypt"
+        message = pad_message(input("Input the message to encrypt: ").encode("utf-8").hex())
+
+        # I am aware this is probably not secure or what happens in practice. Choosing the key is not in the
+        # specification, so I thought to make the key this because it was the first thing that came to mind and was
+        # easy to implement.
+        key = input("Input the key you'd like to use to encrypt the message: ").encode("utf-8")
+        key = hashlib.sha256(key).hexdigest()[:16]
+
+        return choice, message, key
+
+    elif choice == "2":
+        print("Decrypt not implemented yet :(")
+        exit(1)
 
 
-def preliminaries():
-    global xCrypt
-    global message
-    global key
-
-    xCryptPrompt = input("Would you like to: \n[1] encrypt a message \nOR \n[2] decrypt a message?\n")
-
-    if xCryptPrompt == "1":
-        xCrypt = "en"
-        message = input("Input the message to encrypt: ")
-        encKey = input("Input the 128 bit key you'd like to use to encrypt the message: ")
-        keyMatch = re.match(r"^[A-Fa-f0-9]{32}$", encKey)
-        if not keyMatch == None:
-            key = encKey.upper()
-        else:
-            print("Invalid key")
-
-    elif xCryptPrompt == "2":
-        xCrypt = "de"
-        message = input("Input the message to decrypt: ")  # MAYBE HAVE TO
-        decKey = input("Input the 128 bit key you'd like to use to encrypt the message: ")
-        keyMatch = re.match(r"^[A-Fa-f0-9]{32}$", decKey)
-        if not keyMatch == None:
-            key = decKey.upper()
-        else:
-            print("Invalid key")
-
-    else:
-        print("Please enter 1 or 2 to encrypt or decrypt a message, respectively.\n")
-        preliminaries()
+def pad_message(string):
+    msg_length = len(string)
+    pad_length = 0 if msg_length % 16 == 0 else ((msg_length // 16) + 1) * 16
+    return '{stringToPad:{fill}<{width}}'.format(stringToPad=string, fill='0', width=pad_length)
 
 
-def padMessage(string_):
-    msgLength = len(string_)
-    padLength = 0 if msgLength % 16 == 0 else ((msgLength // 16) + 1) * 16
-    return '{stringToPad:{fill}<{width}}'.format(stringToPad=string_, fill='0', width=padLength)
+def blockify_message(string):
+    return [string[i:i + 16] for i in range(0, len(string), 16)]
 
 
-def blockifyMessage(string_):
-    return [string_[i:i + 16] for i in range(0, len(string_), 16)]
+def tokenize(string, n):
+    return [string[i:i + n] for i in range(0, len(string), n)]
 
 
-def tokenize(string_, n):
-    return [string_[i:i + n] for i in range(0, len(string_), n)]
+def hexify_and_matrix_msg(msg):
+    msg_rows, msg_cols = 4, 4
+    string_matrix = [[[0 for x in range(msg_cols)] for y in range(msg_rows)] for z in range(len(msg))]
+    for i in range(len(msg)):
+        for j in range(len(msg[i])):
+            string_matrix[i][j % msg_rows][j // msg_cols] = int(hex(ord(msg[i][j])), 16)
+    return string_matrix
 
 
-def hexifyAndMatrixMsg(msg_):
-    msgRows, msgCols = 4, 4
-    stringMatrix = [[[0 for x in range(msgCols)] for y in range(msgRows)] for z in range(len(msg_))]
-    for i in range(len(msg_)):
-        for j in range(len(msg_[i])):
-            stringMatrix[i][j % msgRows][j // msgCols] = int(hex(ord(msg_[i][j])), 16)
-    return stringMatrix
-
-
-def hexifyAndMatrixKey(key_):
-    keyRows, keyCols = 4, len(key_) // 4
-    stringMatrix = [[0 for x in range(keyCols)] for y in range(keyRows)]
-    for i in range(len(key_)):
-        stringMatrix[i % keyRows][i // keyRows] = int(key_[i], 16)
-    return stringMatrix
+def hexify_and_matrix_key(key):
+    key_rows, key_cols = 4, len(key) // 4
+    string_matrix = [[0 for x in range(key_cols)] for y in range(key_rows)]
+    for i in range(len(key)):
+        string_matrix[i % key_rows][i // key_rows] = int(key[i], 16)
+    return string_matrix
 
 
 # preliminaries()
 
 
 # if xCrypt == "en":
-# message = padMessage(message)
-# message = blockifyMessage(message)
+# message = pad_message(message)
+# message = blockify_message(message)
 # for i in range(len(message)):                 UNCOMMENT AFTER FINISHED TESTING!
 #    message[i] = tokenize(message[i],1)
-# message = hexifyAndMatrixMsg(message)
+# message = hexify_and_matrix_msg(message)
 # key = tokenize(key,1)
-# key = hexifyAndMatrixKey(key)
+# key = hexify_and_matrix_key(key)
 
 def main():
+    """
     message = [
         [
             ["3F", "EE", "C0", "E3"], ["93", "29", "87", "A8"],
@@ -96,6 +81,19 @@ def main():
         ["42", "69", "54", "21"], ["F5", "79", "C9", "FE"],
         ["8D", "65", "52", "41"], ["96", "9F", "75", "05"]
     ]
+    """
+    choice, message, key = prompt_user()
+
+    # TODO: I think I need to fix the padding. Doesn't seem right.
+    # TODO: bytes.fromhex(message) converts back to msg
+
+    tokenized = tokenize(message, 4)
+    blockified = blockify_message(message)
+    #message = hexify_and_matrix_msg(message)
+    print(message)
+    print(tokenized)
+    print(blockified)
+    exit(0)
 
     for i, block in enumerate(message[0]):
         for j, byte in enumerate(block):
