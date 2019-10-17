43697def7d5790fa3eed27bcd110f8e7e85bfa15
Alex <alexhutman@gmail.com>
2019-10-16 22:23:48 -0400

    fixed padding, blockifying
    still currently not in working order

--- AES128.py
+++ AES128.py
@@ -5,19 +5,25 @@ import Decrypt
 
 
 def prompt_user():
-    choice = input("Would you like to:\n[1] encrypt\nOR\n[2] decrypt a message?\nEnter here: ")
+    choice = input("Would you like to:\n\t[1] encrypt a message\n\t[2] decrypt a message\nEnter here: ")
     while choice not in ["1", "2"]:
-        input("Please choose\n[1] to encrypt a message\nOR\n[2] to decrypt a message.\nEnter here: ")
+        choice = input("Please input either 1 or 2.\n\t[1] to encrypt a message\n\t[2] to decrypt a message.\nEnter "
+                       "here: ")
 
     if choice == "1":
         choice = "encrypt"
-        message = pad_message(input("Input the message to encrypt: ").encode("utf-8").hex())
+        message = input("Input the message to encrypt: ")
+        while not message:
+            message = input("Please enter the message you'd like to encrypt: ")
+        message = pad_message(message).encode("utf-8").hex()
 
         # I am aware this is probably not secure or what happens in practice. Choosing the key is not in the
         # specification, so I thought to make the key this because it was the first thing that came to mind and was
         # easy to implement.
-        key = input("Input the key you'd like to use to encrypt the message: ").encode("utf-8")
-        key = hashlib.sha256(key).hexdigest()[:16]
+        key = input("Input the key you'd like to use to encrypt the message with: ")
+        while not key:
+            key = input("Please enter the key you'd like to use to encrypt the message with: ")
+        key = hashlib.sha256(key.encode("utf-8")).hexdigest()[:32]
 
         return choice, message, key
 
@@ -28,46 +34,31 @@ def prompt_user():
 
 def pad_message(string):
     msg_length = len(string)
-    pad_length = 0 if msg_length % 16 == 0 else ((msg_length // 16) + 1) * 16
+    pad_length = 0 if msg_length % 32 == 0 else ((msg_length // 32) + 1) * 32
     return '{stringToPad:{fill}<{width}}'.format(stringToPad=string, fill='0', width=pad_length)
-
-
-def blockify_message(string):
-    return [string[i:i + 16] for i in range(0, len(string), 16)]
 
 
 def tokenize(string, n):
     return [string[i:i + n] for i in range(0, len(string), n)]
 
 
-def hexify_and_matrix_msg(msg):
-    msg_rows, msg_cols = 4, 4
-    string_matrix = [[[0 for x in range(msg_cols)] for y in range(msg_rows)] for z in range(len(msg))]
-    for i in range(len(msg)):
-        for j in range(len(msg[i])):
-            string_matrix[i][j % msg_rows][j // msg_cols] = int(hex(ord(msg[i][j])), 16)
-    return string_matrix
+def hexify(tokenized_arr):
+    hex_arr = []
+    for token in tokenized_arr:
+        hex_arr.append(int(token, 16))
+    return hex_arr
 
 
-def hexify_and_matrix_key(key):
-    key_rows, key_cols = 4, len(key) // 4
-    string_matrix = [[0 for x in range(key_cols)] for y in range(key_rows)]
-    for i in range(len(key)):
-        string_matrix[i % key_rows][i // key_rows] = int(key[i], 16)
-    return string_matrix
+def blockify_and_matrix_msg(tokenized_msg):
+    blockified_msg = []
+    for i in range(len(tokenized_msg) // 16):
+        blockified_msg.append([])
+        for j in range(4):
+            start_index = 4 * (4 * i + j)
+            end_index = 4 * (4 * i + j + 1)
+            blockified_msg[i].append(tokenized_msg[start_index:end_index])
+    return blockified_msg
 
-
-# preliminaries()
-
-
-# if xCrypt == "en":
-# message = pad_message(message)
-# message = blockify_message(message)
-# for i in range(len(message)):                 UNCOMMENT AFTER FINISHED TESTING!
-#    message[i] = tokenize(message[i],1)
-# message = hexify_and_matrix_msg(message)
-# key = tokenize(key,1)
-# key = hexify_and_matrix_key(key)
 
 def main():
     """
@@ -84,26 +75,22 @@ def main():
     """
     choice, message, key = prompt_user()
 
-    # TODO: I think I need to fix the padding. Doesn't seem right.
-    # TODO: bytes.fromhex(message) converts back to msg
+    # TODO: For decryption (whenever that time comes), bytes.fromhex(message).decode('utf-8') converts back to msg
 
-    tokenized = tokenize(message, 4)
-    blockified = blockify_message(message)
-    #message = hexify_and_matrix_msg(message)
     print(message)
-    print(tokenized)
-    print(blockified)
-    exit(0)
+    tokenized_msg = tokenize(message, 2)
+    hexified_msg = hexify(tokenized_msg)
+    blockified_msg = blockify_and_matrix_msg(hexified_msg)
+    print(blockified_msg)
 
-    for i, block in enumerate(message[0]):
-        for j, byte in enumerate(block):
-            message[0][i][j] = int(byte, 16)
+    print(key)
+    tokenized_key = tokenize(key, 2)
+    hexified_key = hexify(tokenized_key)
+    blockified_key = blockify_and_matrix_msg(hexified_key)
+    print(blockified_key)
+    print("-------------------------")
 
-    for i, block in enumerate(key):
-        for j, byte in enumerate(block):
-            key[i][j] = int(byte, 16)
-
-    Encrypt.encrypt(message, key)
+    #Encrypt.encrypt(message, key)
 
 
 if __name__ == "__main__":
